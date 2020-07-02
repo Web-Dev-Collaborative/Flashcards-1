5ab330285cde4a0a397836708c6a11409308bff5
German Henriquez <germanandres.hv@gmail.com>
2020-07-02 10:39:00 -0400

    Flashcard view and create added

--- flashcards/flashes/models.py
+++ flashcards/flashes/models.py
@@ -1,15 +1,22 @@
 from django.db import models
-from django.utils.datetime_safe import datetime
+from django.urls import reverse
+from datetime import datetime
 from django.conf import settings
 
 
 class Flash(models.Model):
     """ Flashcard model """
-    title = models.CharField('Flashcard title', max_length=200)
-    content = models.TextField('Contents')
-    last_change_date = models.DateTimeField('Creation date', default=datetime.now)
+    title = models.CharField('Title', max_length=200)
+    content = models.TextField('Content')
+    last_change_date = models.DateTimeField('Last modified on', default=datetime.now)
 
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('flashes:detail', kwargs={"username": self.created_by, "pk": self.pk})
--- flashcards/flashes/views.py
+++ flashcards/flashes/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.utils.datetime_safe import datetime
+from django.views.generic import CreateView, ListView, DetailView
+from flashcards.flashes.models import Flash
 
 
-class FlashcardListView(TemplateView):
+class FlashcardListView(LoginRequiredMixin, ListView):
+    login_url = '/account/login'
     template_name = 'flashes/list.html'
+    model = Flash
 
 
-class FlashcardCreateView(TemplateView):
+class FlashcardDetailView(LoginRequiredMixin, DetailView):
+    login_url = '/account/login'
+    template_name = 'flashes/detail.html'
+    model = Flash
+    
+    def get_queryset(self):
+        """Only allows the creator to see the flashcard"""
+        qs = super().get_queryset().filter(created_by=self.request.user)
+        return qs
+
+
+class FlashcardCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/account/login'
     template_name = 'flashes/create.html'
+    model = Flash
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        form.instance.last_change_date = datetime.now()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('flashes:list')
