72d8e00647e3c4a1bc888a3e4ff4585829efc426
German Henriquez <germanandres.hv@gmail.com>
2020-07-04 19:30:26 -0400

    test models added

---
+++ flashcards/flashes/tests/test_models.py
@@ -0,0 +1,11 @@
+from flashcards.flashes.models import Flash
+
+
+def test_flash_get_absolute_url():
+    flash = Flash(id=5, title='Test flashcard', content='something not important')
+    assert flash.get_absolute_url() == '/flashcards/5'
+
+
+def test_flash_string():
+    flash = Flash(id=5, title='Test flashcard', content='something not important')
+    assert flash.title == str(flash) == 'Test flashcard'
