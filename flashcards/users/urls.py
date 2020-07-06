3367671dfbb09d8719d242b545e3af10d5551937
German Henriquez <germanandres.hv@gmail.com>
2020-07-05 20:39:33 -0400

    Fixed password change

--- flashcards/users/urls.py
+++ flashcards/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -14,6 +14,7 @@ urlpatterns = [
                                                 extra_context={'action': 'Login'}),
          name='login'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/loginpassword.html',
+                                                                   success_url=reverse_lazy('home'),
                                                                    extra_context={'action': 'Change password'}),
          name='password_change')
 ]
