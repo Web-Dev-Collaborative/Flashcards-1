e85fff0299102a824f54e815e9675c1bc7854fe6
German Henriquez <germanandres.hv@gmail.com>
2020-07-07 19:15:22 -0400

    changed to class based tests

--- flashcards/users/tests/test_views.py
+++ flashcards/users/tests/test_views.py
@@ -1,5 +1,5 @@
 import pytest
-from django.test import Client
+from django.test import Client, TestCase, RequestFactory
 from django.urls import reverse
 from pytest_django.asserts import assertContains
 
@@ -8,36 +8,34 @@ from flashcards.users.views import SignupView
 pytestmark = pytest.mark.django_db
 
 
-def test_signup_view_get(rf):
-    url = reverse("users:signup")
-    request = rf.get(url)
-    response = SignupView.as_view()(request)
-    assertContains(response, 'Username')
-    assertContains(response, 'Password')
-    assertContains(response, 'Password confirmation')
+class TestSignUpview(TestCase):
+    def setUp(self):
+        self.url = reverse("users:signup")
+        self.client = Client()
+        self.factory = RequestFactory()
 
+    def test_signup_view_get(self):
+        response = SignupView.as_view()(self.factory.get(self.url))
+        assertContains(response, 'Username')
+        assertContains(response, 'Password')
+        assertContains(response, 'Password confirmation')
 
-def test_signup_view_post_with_valid_form():
-    url = reverse("users:signup")
-    form = {
-        'username': 'ImJustATestingUser',
-        'email': 'testinguser@testingserver.com',
-        'password1': 'testing321',
-        'password2': 'testing321'
-    }
-    c = Client()
-    response = c.post(url, form)
-    assert response.status_code == 302
+    def test_signup_view_post_with_valid_form(self):
+        form = {
+            'username': 'ImJustATestingUser',
+            'email': 'testinguser@testingserver.com',
+            'password1': 'testing321',
+            'password2': 'testing321'
+        }
+        response = self.client.post(self.url, form)
+        assert response.status_code == 302
 
-
-def test_signup_view_post_with_invalid_form():
-    url = reverse("users:signup")
-    form = {
-        'username': 'ImJustATestingUser',
-        'email': 'testinguser@testingserver.com',
-        'password1': 'apassword',
-        'password2': 'adifferentpassword'
-    }
-    c = Client()
-    response = c.post(url, form)
-    assert response.status_code == 200
+    def test_signup_view_post_with_invalid_form(self):
+        form = {
+            'username': 'ImJustATestingUser',
+            'email': 'testinguser@testingserver.com',
+            'password1': 'apassword',
+            'password2': 'adifferentpassword'
+        }
+        response = self.client.post(self.url, form)
+        assert response.status_code == 200
