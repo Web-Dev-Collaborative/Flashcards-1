08f53a287cfb1dad8f07c3cc5412ccbb37272cc7
German Henriquez <germanandres.hv@gmail.com>
2020-07-18 18:53:06 -0400

    Tests finished

--- flashcards/leitner/tests/test_leitner_views.py
+++ flashcards/leitner/tests/test_leitner_views.py
@@ -174,7 +174,7 @@ class TestCardRelatedViews(TestCase):
         self.assertRaises(Card.DoesNotExist, card.refresh_from_db)
 
 
-# noinspection DuplicatedCode
+# noinspection DuplicatedCode,PyTypeChecker
 class TestSessionRelatedViews(TestCase):
 
     def setUp(self) -> None:
@@ -230,22 +230,153 @@ class TestSessionRelatedViews(TestCase):
         response = self.client.get(self.study_session_url)
         self.assertEqual(response.status_code, 302)
 
+    def test_session_cards_view_redirect_when_no_cards_left(self):
+        """ Asserts the session is marked as finished if there are no cards left.
+        Also asserts the get method redirects to another page """
+        # Because self.box2 is empty, current card should be None
+        session = Session.objects.create(deck=self.deck, current_box=self.box2,
+                                         total_cards_on_box=10)
+        url = reverse('leitner:session-cards', args=(self.deck.pk,))
+
+        response = self.client.get(url)
+        session.refresh_from_db()
+
+        self.assertTrue(session.is_finished)
+        self.assertEqual(response.status_code, 302)
+
     # POST method tests
 
-    def test_session_start_view_post_with_valid_form(self):
-        pass
+    def test_session_start_view_post_with_valid_form_and_non_empty_box(self):
+        """ Asserts a session was created after POST method, given a valid form
+        and a non empty box was selected """
+        last_session_before = Session.objects.last()
+        url = reverse('leitner:session', args=(self.deck.pk,))
+        data = {'current_box': self.box1.pk}
+
+        self.client.post(url, data=data)
+        last_session_after = Session.objects.last()
+
+        self.assertNotEqual(last_session_before, last_session_after)
+
+    def test_session_start_view_post_with_valid_form_and_empty_box(self):
+        """ Asserts a session was NOT created after POST method, given a valid form
+        and an EMPTY box was selected """
+        last_session_before = Session.objects.last()
+        url = reverse('leitner:session', args=(self.deck.pk,))
+        data = {'current_box': self.box2.pk}
+
+        self.client.post(url, data=data)
+        last_session_after = Session.objects.last()
+
+        self.assertEqual(last_session_before, last_session_after)
 
     def test_session_start_view_with_invalid_form(self):
-        pass
+        """ Asserts a session was NOT created after POST method, given an invalid form"""
+        last_session_before = Session.objects.last()
+        url = reverse('leitner:session', args=(self.deck.pk,))
+        data = {'current_box': ''}
+
+        self.client.post(url, data=data)
+        last_session_after = Session.objects.last()
+
+        self.assertEqual(last_session_before, last_session_after)
 
     def test_session_cards_view_with_correct_answer_post(self):
-        pass
+        """ Asserts the card is correctly moved after a correct answer"""
+        data = {'_correct': 'Yes'}
+        card = Card.objects.create(
+            on_deck=self.deck, on_box=self.box2, front_text='blah', back_text='blah'
+        )
+        # Box 2 was empty before creating this card, so this should be the current card
+        Session.objects.create(deck=self.deck, current_box=self.box2, total_cards_on_box=10)
+        url = reverse('leitner:session-cards', args=(self.deck.pk,))
+
+        self.client.post(url, data=data)
+        # If this POST was valid, card should've moved to self.box3
+        card.refresh_from_db()
+
+        self.assertEqual(card.on_box, self.box3)
 
     def test_session_cards_view_with_incorrect_answer_post(self):
-        pass
+        """ Asserts the card is correctly moved after an incorrect answer """
+        data = {'_incorrect': 'No'}
+        card = Card.objects.create(
+            on_deck=self.deck, on_box=self.box2, front_text='blah', back_text='blah'
+        )
+        # Box 2 was empty before creating this card, so this should be the current card
+        Session.objects.create(deck=self.deck, current_box=self.box2, total_cards_on_box=10)
+        url = reverse('leitner:session-cards', args=(self.deck.pk,))
 
-    def test_session_cards_view_with_invalid_post(self):
-        pass
+        self.client.post(url, data=data)
+        # If this POST was valid, card should've moved to self.box1
+        card.refresh_from_db()
+
+        self.assertEqual(card.on_box, self.box1)
 
     def test_session_finished_view_post_method(self):
-        pass
+        """ Asserts the session is deleted after POST method and the box is set not in session"""
+        url = reverse('leitner:session-finished', args=(self.deck.pk,))
+        session = Session.objects.create(deck=self.deck, current_box=self.box1, total_cards_on_box=10, is_finished=True)
+        self.box1.in_session = True
+        self.box1.save()
+
+        self.client.post(url)
+        self.box1.refresh_from_db()
+
+        self.assertRaises(Session.DoesNotExist, session.refresh_from_db)
+        self.assertFalse(self.box1.in_session)
+
+    # POST methods that should return HTTP 403
+
+    def test_session_start_view_post_with_existing_session(self):
+        """ Asserts there is an HTTP 403 after trying to start a new session """
+        url = reverse('leitner:session', args=(self.deck.pk,))
+        Session.objects.create(deck=self.deck, current_box=self.box1, total_cards_on_box=10)
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_session_cards_view_post_with_no_session(self):
+        """ Asserts HTTP 403 after trying to POST an answer with no current session"""
+        url = reverse('leitner:session-cards', args=(self.deck.pk,))
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_session_cards_view_post_with_finished_session(self):
+        """ Asserts HTTP 403 after trying to finish unfinished session """
+        url = reverse('leitner:session-cards', args=(self.deck.pk,))
+        Session.objects.create(deck=self.deck, current_box=self.box1, total_cards_on_box=10, is_finished=True)
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_session_cards_view_post_with_invalid_post(self):
+        """ Asserts HTTP 403 after trying to give invalid answer """
+        url = reverse('leitner:session-cards', args=(self.deck.pk,))
+        Session.objects.create(deck=self.deck, current_box=self.box1, total_cards_on_box=10)
+        data = {'asdgadg': 'asdhajkl'}
+
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_session_finished_view_with_no_session(self):
+        """ Asserts HTTP 403 after trying to finish an unexisting session """
+        url = reverse('leitner:session-finished', args=(self.deck.pk,))
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_session_finished_view_with_non_finished_session(self):
+        """ Asserts HTTP 403 after trying to finish an unfinished session with remaining cards"""
+        url = reverse('leitner:session-finished', args=(self.deck.pk,))
+        Session.objects.create(deck=self.deck, current_box=self.box1, total_cards_on_box=10, is_finished=False)
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 403)
